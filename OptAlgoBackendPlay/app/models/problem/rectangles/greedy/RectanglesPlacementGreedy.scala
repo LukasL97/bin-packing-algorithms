90a80fb01cc1c2c569b2ba12a211a52b009dfed9
Lukas Lehmann <lukas.lehmann97@gmail.com>
2021-02-26 14:41:11 +0100

    implement top left first strategy for rectangles placement selection for greedy

--- OptAlgoBackendPlay/app/models/problem/rectangles/greedy/RectanglesPlacementGreedy.scala
+++ OptAlgoBackendPlay/app/models/problem/rectangles/greedy/RectanglesPlacementGreedy.scala
@@ -8,13 +8,15 @@ import models.problem.rectangles.Placing
 import models.problem.rectangles.Rectangle
 import models.problem.rectangles.RectanglesPlacement
 import models.problem.rectangles.RectanglesPlacementSolution
+import models.problem.rectangles.RectanglesPlacementSolutionValidator
 
 trait RectanglesPlacementGreedy extends RectanglesPlacement {
   val selectionHandler: RectanglesPlacementSelectionHandler
   lazy val greedy = new Greedy[Rectangle, RectanglesPlacementSolution](selectionHandler)
 }
 
-trait RectanglesPlacementSelectionHandler extends SelectionHandler[Rectangle, RectanglesPlacementSolution] {
+trait RectanglesPlacementSelectionHandler
+    extends SelectionHandler[Rectangle, RectanglesPlacementSolution] with RectanglesPlacementSolutionValidator {
 
   val boxLength: Int
 
@@ -24,11 +26,58 @@ trait RectanglesPlacementSelectionHandler extends SelectionHandler[Rectangle, Re
     candidate: Rectangle,
     solution: RectanglesPlacementSolution
   ): RectanglesPlacementSolution = {
-    // TODO: proper placing algorithm
-    val maxBoxId = solution.placement.values.map(_.box.id).max
+    val placementsPerBox = solution.placement.groupBy {
+      case (rectangle, placing) => placing.box
+    }.toSeq.sortBy {
+      case (box, placement) => box.id
+    }.map {
+      case (box, placement) => box -> placement.map { case (rectangle, placing) => rectangle -> placing.coordinates }
+    }
     RectanglesPlacementSolution(
-      solution.placement + (candidate -> Placing(Box(maxBoxId + 1, boxLength, boxLength), Coordinates(0, 0)))
+      solution.placement + (candidate -> placeRectangleInFirstPossiblePosition(candidate, placementsPerBox))
     )
   }
+
+  private def placeRectangleInFirstPossiblePosition(
+    rectangle: Rectangle,
+    placementsPerBox: Seq[(Box, Map[Rectangle, Coordinates])]
+  ): Placing = {
+    val maxBoxId = placementsPerBox.toMap.keys.map(_.id).max
+    placementsPerBox
+      .foldLeft(Option.empty[Placing]) {
+        case (foundPlacing, (box, placement)) =>
+          foundPlacing.orElse(
+            placeRectangleInBoxAtMostTopLeftPoint(rectangle, placement)
+              .map(Placing(box, _))
+          )
+      }
+      .getOrElse(
+        Placing(
+          Box(maxBoxId + 1, boxLength, boxLength),
+          Coordinates(0, 0)
+        )
+      )
+  }
+
+  private def placeRectangleInBoxAtMostTopLeftPoint(
+    rectangle: Rectangle,
+    placement: Map[Rectangle, Coordinates]
+  ): Option[Coordinates] = {
+    coordinatesInTopLeftFirstOrder.find { coordinates =>
+      val newPlacement = placement + (rectangle -> coordinates)
+      allRectanglesInBoundsForSingleBox(newPlacement, boxLength) && allRectanglesDisjunctiveInSingleBox(newPlacement)
+    }
+  }
+
+  private implicit val topLeftFirstOrdering: Ordering[Coordinates] = (left: Coordinates, right: Coordinates) => {
+    (left.x + left.y) - (right.x + right.y) match {
+      case 0 => left.x - right.x
+      case other => other
+    }
+  }
+
+  private lazy val coordinatesInTopLeftFirstOrder = (0 until boxLength)
+    .flatMap(x => (0 until boxLength).map(y => Coordinates(x, y)))
+    .sorted
 
 }
---
+++ OptAlgoBackendPlay/test/models/problem/rectangles/greedy/RectanglesPlacementSelectionHandlerSpec.scala
@@ -0,0 +1,58 @@
+package models.problem.rectangles.greedy
+
+import models.problem.rectangles.Box
+import models.problem.rectangles.Coordinates
+import models.problem.rectangles.Placing
+import models.problem.rectangles.Rectangle
+import models.problem.rectangles.RectanglesPlacementSolution
+import org.scalatest.MustMatchers
+import org.scalatest.WordSpec
+
+class RectanglesPlacementSelectionHandlerSpec extends WordSpec with MustMatchers {
+
+  private class RectanglesPlacementSelectionHandlerImpl(
+    override val boxLength: Int,
+    override val candidates: Iterable[Rectangle]
+  ) extends RectanglesPlacementSelectionHandler {
+    override def selectNextCandidate(candidates: Iterable[Rectangle]): (Rectangle, Iterable[Rectangle]) = {
+      (candidates.head, candidates.tail)
+    }
+  }
+
+  private val boxLength = 5
+  private val selectionHandler = new RectanglesPlacementSelectionHandlerImpl(boxLength, Seq.empty)
+
+  "RectanglesPlacementSelectionHandler" should {
+
+    "place a candidate in the first possible position according to the top left strategy" when {
+
+      val solution = RectanglesPlacementSolution(Map(
+        Rectangle(1, 5, 3) -> Placing(Box(1, 5, 5), Coordinates(0, 0)),
+        Rectangle(2, 3, 2) -> Placing(Box(1, 5, 5), Coordinates(0, 3)),
+        Rectangle(3, 2, 1) -> Placing(Box(2, 5, 5), Coordinates(0, 0))
+      ))
+
+      "given a candidate fitting in the first box" in {
+        val candidate = Rectangle(4, 1, 2)
+        selectionHandler.placeCandidateInSolution(candidate, solution).placement mustEqual {
+          solution.placement + (candidate -> Placing(Box(1 ,5 ,5), Coordinates(3, 3)))
+        }
+      }
+
+      "given a candidate not fitting in the first but in the second box" in {
+        val candidate = Rectangle(4, 3, 2)
+        selectionHandler.placeCandidateInSolution(candidate, solution).placement mustEqual {
+          solution.placement + (candidate -> Placing(Box(2 ,5 ,5), Coordinates(0, 1)))
+        }
+      }
+
+      "given a candidate not fitting in any currently used box" in {
+        val candidate = Rectangle(4, 4, 5)
+        selectionHandler.placeCandidateInSolution(candidate, solution).placement mustEqual {
+          solution.placement + (candidate -> Placing(Box(3 ,5 ,5), Coordinates(0, 0)))
+        }
+      }
+    }
+  }
+
+}
