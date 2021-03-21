94426d1b4b96a6cc8077d976df8f1e0df80f69cb
Lukas Lehmann <lukas.lehmann97@gmail.com>
2021-03-21 14:14:01 +0100

    ls-geometry specific metrics analysis

--- metrics-analysis/step_duration_analysis.py
+++ metrics-analysis/step_duration_analysis.py
@@ -13,11 +13,12 @@ def query_metrics(endpoint, body):
 
 
 def get_run_step_name(algorithm):
-    run_step_names = {
-        'greedy': 'greedy-run-step',
-        'localsearch': 'local-search-run-step'
-    }
-    return run_step_names[algorithm]
+    if algorithm.startswith('greedy'):
+        return 'greedy-run-step'
+    elif algorithm.startswith('ls'):
+        return 'local-search-run-step'
+    else:
+        raise RuntimeError('Runstep name unknown for algorithm %s' % algorithm)
 
 
 def build_query_body(name, run_id):
@@ -67,14 +68,42 @@ def analyze_step_durations(run_id, algorithm, endpoint):
         key=lambda x: x[0]
     )
     plot(step_durations)
-    print('Step durations sum: %r seconds' % (sum([time for (step, time) in step_durations]) / 1000))
+    print('Step durations sum: %r ms' % (sum([time for (step, time) in step_durations])))
+
+
+def analyze_timer(name, runId, endpoint):
+    query = build_query_body(name, runId)
+    metric = query_metrics(endpoint, query)[0]
+    return metric['result']['count'], metric['result']['mean'] / 1000000
+
+
+def analyze_local_search_geometry_based(run_id, endpoint):
+    timers = [
+        'ls-geometry-evaluate',
+        'box-merge-neighborhood',
+        'box-pull-up-neighborhood',
+        'coarse-multiple-box-pull-up-neighborhood',
+        'entire-box-maximally-shifted-neighborhood'
+    ]
+    for timer in timers:
+        count, time = analyze_timer(timer, run_id, endpoint)
+        print('%s: count: %d, mean: %r ms, sum: %r ms' % (timer, count, time, count * time))
 
 
 if __name__ == '__main__':
+    algorithms = [
+        'greedy_random',
+        'greedy_size',
+        'ls_geometry',
+        'ls_overlap'
+    ]
     parser = argparse.ArgumentParser()
     parser.add_argument('--runId', type=str, required=True)
-    parser.add_argument('--algorithm', type=str, default='localsearch', choices=['greedy', 'localsearch'])
+    parser.add_argument('--algorithm', type=str, default='ls_geometry', choices=algorithms)
     parser.add_argument('--endpoint', type=str, default='http://localhost:9000/metrics')
     args = parser.parse_args()
 
     analyze_step_durations(args.runId, args.algorithm, args.endpoint)
+
+    if (args.algorithm == 'ls_geometry'):
+        analyze_local_search_geometry_based(args.runId, args.endpoint)
