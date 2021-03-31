b0ceb2a5db839bb540a835776d4bc19facc648b1
Lukas Lehmann <lukas.lehmann97@gmail.com>
2021-03-31 19:32:11 +0200

    use ExceededOverlapOutsourcingNeighborhood and OverlappingBoxWeightedScore in TopLeftFirstOverlappingBinPacking

--- bin-packing-backend-play/app/models/problem/binpacking/localsearch/TopLeftFirstOverlappingBinPacking.scala
+++ bin-packing-backend-play/app/models/problem/binpacking/localsearch/TopLeftFirstOverlappingBinPacking.scala
@@ -2,6 +2,9 @@ package models.problem.binpacking.localsearch
 
 import models.algorithm.OneDimensionalScore
 import models.algorithm.Score
+import models.problem.binpacking.localsearch.evaluation.BoxWeightedScore
+import models.problem.binpacking.localsearch.evaluation.BoxWeightedTopLeftFirstEvaluation
+import models.problem.binpacking.localsearch.neighborhood.ExceededOverlapOutsourcingNeighborhood
 import models.problem.binpacking.solution.OverlappingTopLeftFirstBinPackingSolution
 import models.problem.binpacking.solution.Rectangle
 
@@ -20,18 +23,51 @@ class TopLeftFirstOverlappingBinPacking(
 
 class TopLeftFirstOverlappingBinPackingSolutionHandler(
   rectangles: Set[Rectangle],
-  boxLength: Int
-) extends BinPackingSolutionHandler[OverlappingTopLeftFirstBinPackingSolution] {
+  override val boxLength: Int
+) extends BinPackingSolutionHandler[OverlappingTopLeftFirstBinPackingSolution]
+    with BoxWeightedTopLeftFirstEvaluation {
 
   override val startSolution: OverlappingTopLeftFirstBinPackingSolution =
     OverlappingTopLeftFirstBinPackingSolution.apply(rectangles.toSeq, boxLength, 1.0)
 
+  private val exceededOverlapOutsourcingNeighborhood = new ExceededOverlapOutsourcingNeighborhood(boxLength)
+
   override def getNeighborhood(
     solution: OverlappingTopLeftFirstBinPackingSolution,
     step: Int
-  ): View[OverlappingTopLeftFirstBinPackingSolution] = View.empty
+  ): View[OverlappingTopLeftFirstBinPackingSolution] = {
+    val solutionsWithOutsourcedRectangles = exceededOverlapOutsourcingNeighborhood
+      .createExceededOverlapOutsourcingNeighborhood(solution, maxAllowedOverlap(step))
+    solutionsWithOutsourcedRectangles
+  }
 
   override def evaluate(solution: OverlappingTopLeftFirstBinPackingSolution, step: Int): Score = {
-    OneDimensionalScore(0)
+    OverlappingBoxWeightedScore(
+      evaluate(solution),
+      OneDimensionalScore(solution.getExceededOverlapCount(maxAllowedOverlap(step)))
+    )
+  }
+
+  private def maxAllowedOverlap(step: Int): Double = {
+    Math.max(1.0 - step * 0.01, 0.0)
+  }
+
+  override def stopOnStagnation(solution: OverlappingTopLeftFirstBinPackingSolution): Boolean = {
+    solution.overlappings.forall {
+      case (_, boxOverlappings) => boxOverlappings.isEmpty
+    }
+  }
+}
+
+case class OverlappingBoxWeightedScore(
+  objectiveScore: BoxWeightedScore,
+  overlapPenalty: OneDimensionalScore
+) extends Score {
+  override def compare(that: Score): Int = {
+    val other = that.asInstanceOf[OverlappingBoxWeightedScore]
+    overlapPenalty.compareTo(other.overlapPenalty) match {
+      case 0 => objectiveScore.compareTo(other.objectiveScore)
+      case result => result
+    }
   }
 }
