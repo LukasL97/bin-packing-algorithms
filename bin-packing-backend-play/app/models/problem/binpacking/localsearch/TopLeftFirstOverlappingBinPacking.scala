a40e1ff03b5c9bdbdc5664d15e0cc2ef553a0cb6
Lukas Lehmann <lukas.lehmann97@gmail.com>
2021-03-31 17:55:50 +0200

    start implementing TopLeftFirstOverlappingBinPacking without proper neighborhood and evaluation yet

---
+++ bin-packing-backend-play/app/models/problem/binpacking/localsearch/TopLeftFirstOverlappingBinPacking.scala
@@ -0,0 +1,36 @@
+package models.problem.binpacking.localsearch
+
+import models.algorithm.OneDimensionalScore
+import models.algorithm.Score
+import models.problem.binpacking.solution.OverlappingTopLeftFirstBinPackingSolution
+import models.problem.binpacking.solution.Rectangle
+
+import scala.collection.View
+
+class TopLeftFirstOverlappingBinPacking(
+  override val boxLength: Int,
+  override val numRectangles: Int,
+  override val rectangleWidthRange: (Int, Int),
+  override val rectangleHeightRange: (Int, Int)
+) extends BinPackingLocalSearch[OverlappingTopLeftFirstBinPackingSolution] {
+
+  override val solutionHandler: BinPackingSolutionHandler[OverlappingTopLeftFirstBinPackingSolution] =
+    new TopLeftFirstOverlappingBinPackingSolutionHandler(rectangles, boxLength)
+}
+
+class TopLeftFirstOverlappingBinPackingSolutionHandler(
+  rectangles: Set[Rectangle],
+  boxLength: Int
+) extends BinPackingSolutionHandler[OverlappingTopLeftFirstBinPackingSolution] {
+
+  override val startSolution: OverlappingTopLeftFirstBinPackingSolution =
+    OverlappingTopLeftFirstBinPackingSolution.apply(rectangles.toSeq, boxLength, 1.0)
+
+  override def getNeighborhood(
+    solution: OverlappingTopLeftFirstBinPackingSolution
+  ): View[OverlappingTopLeftFirstBinPackingSolution] = View.empty
+
+  override def evaluate(solution: OverlappingTopLeftFirstBinPackingSolution, step: Int): Score = {
+    OneDimensionalScore(0)
+  }
+}
