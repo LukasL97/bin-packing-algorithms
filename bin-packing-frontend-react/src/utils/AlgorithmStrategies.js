09588a390814955dd1ca5862ae8616aa098787d6
Lukas Lehmann <lukas.lehmann97@gmail.com>
2021-04-06 10:10:07 +0200

    comment out unused algorithm strategies

--- bin-packing-frontend-react/src/utils/AlgorithmStrategies.js
+++ bin-packing-frontend-react/src/utils/AlgorithmStrategies.js
@@ -2,37 +2,37 @@ class AlgorithmStrategies {
 
   static getAll() {
     return [
-      {
-        id: 'greedy randomSelection',
-        name: 'Greedy - Random Selection'
-      },
-      {
-        id: 'greedy sizeOrdered',
-        name: 'Greedy - Size-ordered'
-      },
-      {
-        id: 'greedy2 randomSelection',
-        name: 'Quick Greedy - Random Selection'
-      },
+      // {
+      //   id: 'greedy randomSelection',
+      //   name: 'Greedy - Random Selection'
+      // },
+      // {
+      //   id: 'greedy sizeOrdered',
+      //   name: 'Greedy - Size-ordered'
+      // },
+      // {
+      //   id: 'greedy2 randomSelection',
+      //   name: 'Quick Greedy - Random Selection'
+      // },
       {
         id: 'greedy2 sizeOrdered',
-        name: 'Quick Greedy - Size-ordered'
+        name: 'Greedy - Size-ordered'
       },
       {
         id: 'greedy boxClosing',
         name: 'Greedy - Box Closing'
       },
-      {
-        id: 'localSearch geometryBased',
-        name: 'Local Search - Geometry-based'
-      },
-      {
-        id: 'localSearch eventuallyFeasibleGeometryBased',
-        name: 'Local Search - Geometry-based (eventually feasible)'
-      },
+      // {
+      //   id: 'localSearch geometryBased',
+      //   name: 'Local Search - Geometry-based'
+      // },
+      // {
+      //   id: 'localSearch eventuallyFeasibleGeometryBased',
+      //   name: 'Local Search - Geometry-based (eventually feasible)'
+      // },
       {
         id: 'localSearch boxMerging',
-        name: 'Local Search - Box Merging'
+        name: 'Local Search - Geometric'
       },
       {
         id: 'localSearch overlapping',
